6bf98fd6feb85b58660abfd9a54a1e85e72ccd72
leulmes <leulmesfin152025@gmail.com>
2025-07-28 15:21:57 -0400

    finishing touches for v1

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
+import ScrollIntoView from 'react-scroll-into-view'
 
 const Logo = () => {
   return (
-    <span className='font-pixelify ml-4 items-center text-3xl text-white md:ml-10 md:text-5xl'>
-      leuldev
-    </span>
+    <ScrollIntoView selector={"#first-section"} className='cursor-pointer'>
+        <span className='font-pixelify ml-4 items-center text-3xl text-white md:ml-10 md:text-5xl'>
+        leuldev
+      </span>
+    </ScrollIntoView>
+    
   )
 }
 
